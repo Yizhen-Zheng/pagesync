181ddd887b82dfe42cc71b37f5f6525cc09951fa
cabbage <cab226023@gmail.com>
2025-05-18 22:59:05 +0900

    feat: add clerk basic setup

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,8 +1,8 @@
 import NavLink from "@/components/common/nav-link";
 import { FileText } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Header() {
-  const loggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
@@ -16,19 +16,26 @@ export default function Header() {
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center ">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {loggedIn && <NavLink href="/#dashboard">Your summarise</NavLink>}
+        <SignedIn>
+          <NavLink href="/#dashboard">Your summarise</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end lg:flex-1 ">
-        {loggedIn ? (
+        <SignedIn>
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF In</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            <SignedIn>
+              <UserButton></UserButton>
+            </SignedIn>
           </div>
-        ) : (
+        </SignedIn>
+
+        <SignedOut>
           <NavLink href="/sign-in">Sign In</NavLink>
-        )}
+          {/* <SignInButton></SignInButton> */}
+        </SignedOut>
       </div>
     </nav>
   );
